6dff464c952759e9b80f17e530dcd6b14491d4fa
StasKazakov <urgor8@gmail.com>
2025-01-30 12:40:45 +0200

    del nav

--- components/Home.tsx
+++ components/Home.tsx
@@ -4,28 +4,12 @@ import Expertise from './expertise/Expertise'
 import Cases from './cases/Cases'
 import Tools from './tools/Tools'
 import Contacts from './contacts/Contacts'
-import MobNav from './navigation/MobNav'
-import Nav from './navigation/Nav'
-import { useState, useEffect } from 'react';
 
 const Home = () => {
-const [isMobile, setIsMobile] = useState(false);
-
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth <= 1024); 
-    };
-
-    handleResize(); 
-    window.addEventListener('resize', handleResize); 
-
-    return () => window.removeEventListener('resize', handleResize); 
-  }, []);
 
   return (
     <div>
       <Scroll />
-      {isMobile ? <MobNav /> : <Nav />}
       <Hero />
       <Expertise />
       <Cases />
