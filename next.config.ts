b9b39f17b55b9af7a1fdc9fb8d796f81c465cd24
staskazakov <urgor8@gmail.com>
2025-05-17 18:36:39 +0200

    fix form input color

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/contact',
+        destination: 'http://localhost:5400/send-to-bot',
+      },
+      {
+        source: '/api/order',
+        destination: 'http://localhost:5400/vls',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
